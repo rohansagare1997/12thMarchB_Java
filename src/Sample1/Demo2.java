6159456d2e9cc564e67a84f8c0d95b2ab219af59
Rohan <Rohan@DESKTOP-4O4F415>
2022-10-15 17:23:08 +0530

    [Rohan]:added class demo2

---
+++ src/Sample1/Demo2.java
@@ -0,0 +1,15 @@
+package Sample1;
+
+public class Demo2 
+{
+public static void main(String[] args) 
+{
+	System.out.println("hello");
+	
+	System.out.println("Hello");
+	
+	System.out.println("Hii");
+	
+	System.out.println("Hii");
+}
+}
